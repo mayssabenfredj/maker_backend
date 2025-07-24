98900a1382d0724614816de18b35ccc9f652706c
Ubuntu <ubuntu@vps-0664a61b.vps.ovh.net>
2025-07-24 13:24:43 +0000

    server files

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['*','http://localhost:5173', 'http://localhost:3000'],
+    origin: ['*','http://makerskills.tn', 'https://makerskills.tn'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
